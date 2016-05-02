755715cb7fef3ebe101bb9cbfaf2b66f02e1ef0c
myim89 <myim@mail.sfsu.edu>
2016-05-02 08:42:36 -0700

    Edited players to player in rename_columns.rb

--- db/migrate/20160416011840_rename_columns.rb
+++ db/migrate/20160416011840_rename_columns.rb
@@ -2,6 +2,6 @@ class RenameColumns < ActiveRecord::Migration
   def change
    rename_column :locations,:game,:game_id
    rename_column :locations,:player,:player_id
-   add_column :players,:password,:string
+   add_column :player,:password,:string
   end
 end
