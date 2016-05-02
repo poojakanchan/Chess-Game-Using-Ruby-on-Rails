24037886ecf8087c3c9f399c7b70c462dd2f6964
myim89 <myim@mail.sfsu.edu>
2016-05-02 09:12:11 -0700

    Reverted RenameColumns, Added to devise.rb

--- db/migrate/20160416011840_rename_columns.rb
+++ db/migrate/20160416011840_rename_columns.rb
@@ -2,6 +2,6 @@ class RenameColumns < ActiveRecord::Migration
   def change
    rename_column :locations,:game,:game_id
    rename_column :locations,:player,:player_id
-   add_column :player,:password,:string
+   add_column :players,:password,:string
   end
 end
